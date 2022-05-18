a1b87ba3063a1e9fc1eaa7aa1af7ecafe264be32
silous888 <silous666@gmail.com>
2022-05-18 14:38:13 +0200

    Menu fonctionnel

---
+++ EscapeGame/Assets/Scripts/SC_MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_MainMenu : MonoBehaviour
+{
+    public GameObject MainMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuButton();
+    }
+
+    public void ButtonCommencer()
+    {
+        // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Playground");
+    }
+
+    public void MainMenuButton()
+    {
+        // Show Main Menu
+        MainMenu.SetActive(true);
+    }
+
+    public void ButtonQuitter()
+    {
+        // Quit Game
+        Application.Quit();
+    }
+}
