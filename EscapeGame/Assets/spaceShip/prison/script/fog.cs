a28752efd25fbcbdb607fde16592951ea8707970
vinceratopos <vincent.cardinal.1207@outlook.fr>
2022-05-18 14:04:18 +0200

    14h04 début

--- EscapeGame/Assets/spaceShip/prison/script/fog.cs
+++ EscapeGame/Assets/spaceShip/prison/script/fog.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class fog_disable : MonoBehaviour
+public class fog : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
