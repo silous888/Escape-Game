4a20b0f352840717f4a1eb93f788005d553c819b
silous888 <silous666@gmail.com>
2022-05-18 15:35:40 +0200

    suppression Playground, et changement Menu et settings
    setting, changement de l'ordre des scenes dans le build

--- EscapeGame/Assets/Scripts/SC_MainMenu.cs
+++ EscapeGame/Assets/Scripts/SC_MainMenu.cs
@@ -15,7 +15,7 @@ public class SC_MainMenu : MonoBehaviour
     public void ButtonCommencer()
     {
         // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Playground");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("prison");
     }
 
     public void MainMenuButton()
