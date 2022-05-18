976f1d5c96e5e8c0336bd0d6ec8e554ec0d03267
vinceratopos <vincent.cardinal.1207@outlook.fr>
2022-05-18 17:15:51 +0200

    travail dure

--- EscapeGame/Assets/spaceShip/prison/script/object_use.cs
+++ EscapeGame/Assets/spaceShip/prison/script/object_use.cs
@@ -9,18 +9,28 @@ public class object_use : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject cam;
-    public GameObject text;
+    public Text text;
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        if(Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
-        {
-            if(hit.transform.gameObject == gameObject)
-            {
-                if (hit.distance < 5)
-                {
 
+        
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit, 5))
+        {
+            if (hit.transform.gameObject == gameObject)
+            {
+
+
+                gameObject.GetComponent<Outline>().enabled = true;
+
+                text.enabled=true;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    print("e key was pressed");
+                    gameObject.GetComponent<big_doors>().enabled = true;
+
+<<<<<<< Updated upstream
                     gameObject.GetComponent<Outline>().enabled = true;
                 
                     text.SetActive(true);
@@ -30,13 +40,21 @@ public class object_use : MonoBehaviour
                         gameObject.GetComponent<big_doors>().enabled = true;
                         
                     }
+=======
+>>>>>>> Stashed changes
                 }
+
             }
             else
             {
                 gameObject.GetComponent<Outline>().enabled = false;
-                text.SetActive(false);
+                text.enabled = false;
             }
+        }
+        else
+        {
+            gameObject.GetComponent<Outline>().enabled = false;
+            text.enabled = false;
         }
     }
 }
