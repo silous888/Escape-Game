0c9abb543857bd39444ff9b63069f4a50ef10885
vinceratopos <vincent.cardinal.1207@outlook.fr>
2022-05-19 12:52:32 +0200

    test focus pc

---
+++ EscapeGame/Assets/spaceShip/prison/script/cam_focus_pc.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cam_focus_pc : MonoBehaviour
+{
+
+    public GameObject cam_pc;
+    public GameObject cam_player;
+    public GameObject pc;
+    public float smoothSpeed = 0.05f;
+    public float x, y, z;
+
+    private void Start()
+    {
+      
+      
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 dir = cam_pc.transform.TransformDirection(Vector3.forward);
+        dir = dir.normalized;
+        Vector3 desiredPosition = cam_pc.transform.position+dir+ new Vector3(x, y, z);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+        transform.LookAt(pc.transform.position);
+    }
+}
