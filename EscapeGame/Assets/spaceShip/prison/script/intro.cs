4192e6c9de431ac34685396b504cb8edfb13e747
vinceratopos <vincent.cardinal.1207@outlook.fr>
2022-05-19 02:45:26 +0200

    sound_effect

--- EscapeGame/Assets/spaceShip/prison/script/intro.cs
+++ EscapeGame/Assets/spaceShip/prison/script/intro.cs
@@ -4,20 +4,55 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public GameObject laser;
     public float periode;
     bool toggle = false;
     float time_left;
     float time_start;
+
+    public AudioClip impact;
+    public AudioClip boom;
+    public AudioClip zuee;
+    AudioSource audioSource;
+    bool play;
+
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        play = true;
+    }
     // Update is called once per frame
     void Update()
     {
-        time_left += Time.deltaTime;
-        if (time_left > periode)
+        time_start += Time.deltaTime;
+        
+        if(play) audioSource.PlayOneShot(impact, 0.7F);
+
+        if (time_start > 20f)
         {
-            if (toggle) GetComponent<Light>().intensity = 0.0f;
-            else GetComponent<Light>().intensity = 30.0f;
-            time_left = 0f;
-            toggle = !toggle;
+            if (time_start < 25f)
+            {
+                time_left += Time.deltaTime;
+                if (time_left > periode)
+                {
+                    if (toggle)
+                    {
+                        laser.gameObject.SetActive(false);
+                        audioSource.PlayOneShot(zuee, 0.7F);
+                    }
+
+                    else laser.gameObject.SetActive(true);
+                    
+                    time_left = 0f;
+                    toggle = !toggle;
+                }
+            }
+            else
+            {
+                laser.gameObject.SetActive(false);
+            }
         }
+        
     }
 }
