bfc6cd00d6ab551623c6f8c455d57a4f96578181
vinceratopos <vincent.cardinal.1207@outlook.fr>
2022-05-21 17:56:07 +0200

    initialisation cadavre

---
+++ EscapeGame/Assets/Scripts/key_corpse.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+using UnityEngine.InputSystem;
+#endif
+public class key_corpse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cam;
+    public Text text;
+
+    void Update()
+    {
+
+
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit, 5))
+        {
+            if (hit.transform.gameObject == gameObject)
+            {
+
+                gameObject.GetComponent<Outline>().enabled = true;
+
+                text.enabled = true;
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    print("e key was pressed");
+                    //Executer ici le script !
+
+                }
+
+            }
+            else
+            {
+                gameObject.GetComponent<Outline>().enabled = false;
+                text.enabled = false;
+            }
+        }
+        else
+        {
+            gameObject.GetComponent<Outline>().enabled = false;
+            text.enabled = false;
+        }
+
+
+    }
+}
