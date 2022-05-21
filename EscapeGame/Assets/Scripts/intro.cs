be0a7e1538d97df43cf312c4ee01a32ce05547d8
vinceratopos <vincent.cardinal.1207@outlook.fr>
2022-05-21 18:34:53 +0200

    correction bug intro

--- EscapeGame/Assets/Scripts/intro.cs
+++ EscapeGame/Assets/Scripts/intro.cs
@@ -25,27 +25,32 @@ public class intro : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         p_impact = true;
         p_explo = true;
+        
     }
     // Update is called once per frame
     void Update()
     {
         time_start += Time.deltaTime;
-
+        
         if (p_impact)
         {
             audioSource.PlayOneShot(impact, 1.2F);
             p_impact = false;
         }
 
-        if (time_start > 10f)
+        if ((time_start > 10f)&& (time_start < 20f))
         {
-            if (p_explo)
+            
+            if ((p_explo)&& (time_start > 11f))
             {
+
+                gameObject.GetComponent<CameraShake>().enabled = true;
                 audioSource.PlayOneShot(boom, 0.7F);
-                print("shake");
-                cam.GetComponent<CameraShake>().enabled = true;
                 p_explo = false;
             }
+           
+
+
             if ((time_start > 12f) && (time_start < 17f))
             {
 
@@ -64,9 +69,9 @@ public class intro : MonoBehaviour
                     toggle = !toggle;
                 }
             }
-            else
+            else if (time_start > 17f)
             {
-                cam.GetComponent<CameraShake>().enabled = false;
+                gameObject.GetComponent<CameraShake>().enabled = false;
                 laser.gameObject.SetActive(false);
                 alarm.gameObject.SetActive(true);
             }
