2da79140b4e5781b74f5aaae31f69e4bb9f681d8
vinceratopos <vincent.cardinal.1207@outlook.fr>
2022-05-23 13:44:25 +0200

    début inventaire

---
+++ EscapeGame/Assets/inventaire.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class inventaire : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool wrench;
+    public bool key_red;
+    public bool key_green;
+    public bool key_blue;
+    public Image wrench_im;
+    void Start()
+    {
+        wrench = false;
+        key_red=false;
+        key_green=false;
+        key_blue=false;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wrench)wrench_im.enabled = true;
+        else wrench_im.enabled = false;
+
+    }
+}
