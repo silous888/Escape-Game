90e0f896a1321269eed18b06aaf2ac4e0487dee7
vinceratopos <vincent.cardinal.1207@outlook.fr>
2022-05-23 14:20:48 +0200

    inventaire

--- EscapeGame/Assets/inventaire.cs
+++ EscapeGame/Assets/inventaire.cs
@@ -10,6 +10,10 @@ public class inventaire : MonoBehaviour
     public bool key_green;
     public bool key_blue;
     public Image wrench_im;
+    public Image red_im;
+    public Image green_im;
+    public Image blue_im;
+
     void Start()
     {
         wrench = false;
@@ -23,6 +27,16 @@ public class inventaire : MonoBehaviour
     {
         if (wrench)wrench_im.enabled = true;
         else wrench_im.enabled = false;
+
+        if (key_red) red_im.enabled = true;
+        else red_im.enabled = false;
+
+
+        if (key_green) green_im.enabled = true;
+        else green_im.enabled = false;
+
+        if (key_blue) blue_im.enabled = true;
+        else blue_im.enabled = false;
 
     }
 }
