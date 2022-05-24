261085657f71c905fb83e6fbc270c38ba6dff21d
silous888 <silous666@gmail.com>
2022-05-24 22:00:49 +0200

    pc après enigme 2 mieux géré

--- EscapeGame/Assets/Scripts/enigme_2.cs
+++ EscapeGame/Assets/Scripts/enigme_2.cs
@@ -17,14 +17,24 @@ public class enigme_2 : MonoBehaviour
     public TMP_InputField reponse;
 
 
-
+    private void Start()
+    {
+        resolu = false;
+    }
     // Update is called once per frame
     void Update()
     {
         //alt+155 = ø
-        if(reponse.text=="x@P3røCUBE666"){
+        if(reponse.text=="x@P3røCUBE666" && !resolu){
             resolu=true;
+            gameObject.GetComponent<utilisation_pc2>().resolu=true;
             porte_placard.GetComponent<Animation>().Play();
+            interfaceEnigme.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            cam_pc.gameObject.SetActive(false);
+            player.gameObject.SetActive(true);
+            cam_player.gameObject.GetComponent<cam_focus_pc>().enabled = false;
+            gameObject.GetComponent<enigme_2>().enabled = false;
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
--- EscapeGame/Assets/Scripts/utilisation_pc2.cs
+++ EscapeGame/Assets/Scripts/utilisation_pc2.cs
@@ -22,55 +22,56 @@ public class utilisation_pc2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit, 5))
-        {
-            if (hit.transform.gameObject == gameObject)
+        if(!resolu){
+            RaycastHit hit;
+            if (Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit, 5))
             {
-
-                if (cam.GetComponent<cam_focus_pc>().enabled)
+                if (hit.transform.gameObject == gameObject)
                 {
-                    text.enabled = false;
-                    gameObject.GetComponent<Outline>().enabled = false;
+
+                    if (cam.GetComponent<cam_focus_pc>().enabled)
+                    {
+                        text.enabled = false;
+                        gameObject.GetComponent<Outline>().enabled = false;
+                    }
+                    else
+                    {
+                        text.enabled = true;
+                        gameObject.GetComponent<Outline>().enabled = true;
+                        gameObject.GetComponent<enigme_2>().enabled = false;
+                    }
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        interfaceEnigme.SetActive(true);
+                        Cursor.lockState = CursorLockMode.Confined;
+                        print("e key was pressed");
+                        cam.GetComponent<cam_focus_pc>().cam_pc = cam_pc;
+                        cam.GetComponent<cam_focus_pc>().num_enigme = 2;
+                        cam.GetComponent<cam_focus_pc>().y = 1;
+                        cam.GetComponent<cam_focus_pc>().x = -1;
+                        cam.GetComponent<cam_focus_pc>().z = 0;
+                        cam.GetComponent<cam_focus_pc>().pc = gameObject;
+                        gameObject.GetComponent<enigme_2>().enabled = true;
+                        if (!resolu) cam.GetComponent<cam_focus_pc>().enabled = true;
+                        
+
+
+                    }
+
                 }
                 else
                 {
-                    text.enabled = true;
-                    gameObject.GetComponent<Outline>().enabled = true;
-                    gameObject.GetComponent<enigme_2>().enabled = false;
+                    gameObject.GetComponent<Outline>().enabled = false;
+                    text.enabled = false;
                 }
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    interfaceEnigme.SetActive(true);
-                    Cursor.lockState = CursorLockMode.Confined;
-                    print("e key was pressed");
-                    cam.GetComponent<cam_focus_pc>().cam_pc = cam_pc;
-                    cam.GetComponent<cam_focus_pc>().num_enigme = 2;
-                    cam.GetComponent<cam_focus_pc>().y = 1;
-                    cam.GetComponent<cam_focus_pc>().x = -1;
-                    cam.GetComponent<cam_focus_pc>().z = 0;
-                    cam.GetComponent<cam_focus_pc>().pc = gameObject;
-                    gameObject.GetComponent<enigme_2>().enabled = true;
-                    if (!resolu) cam.GetComponent<cam_focus_pc>().enabled = true;
-                    
-
-
-                }
-
             }
             else
             {
                 gameObject.GetComponent<Outline>().enabled = false;
                 text.enabled = false;
             }
-        }
-        else
-        {
-            gameObject.GetComponent<Outline>().enabled = false;
-            text.enabled = false;
-        }
 
-
+        }
 
     }
 }
