323b545ed580ba075b7833eb72b6449ef80dc0d2
Gabe <kazemi.hugo@gmail.com>
2022-05-24 14:52:48 +0200

    OUTRO FINITO

---
+++ EscapeGame/Assets/CamdenCurtisFinal.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamdenCurtisFinal : MonoBehaviour
+{
+
+    public float speed;
+    public float limit;
+    float positionY = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            speed += 0.05f;
+        }
+        if (Input.GetMouseButtonDown(1) && speed > 1f)
+        {
+
+            speed -= 0.05f;
+        }
+
+        transform.Translate(0, -speed, 0);
+
+        positionY = transform.position.y;
+
+        if (positionY < limit)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+            print("Ici Camden Curtis, au rapport");
+        }
+    }
+
+}
